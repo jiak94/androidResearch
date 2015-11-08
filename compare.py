3890d2a5796dabc63d743f95b4ef8be45d9ddec7
Jiakuan Li <jiak94@uga.edu>
2015-11-07 20:48:28 -0500

    combine static analysis and runtime analysis

--- compare.py
+++ compare.py
@@ -1,43 +1,47 @@
 __author__ = 'jiakuan'
 import os, sys
 
+
 def add_runtime_output():
-	for file in os.listdir(runtime_path):
-		file_name = runtime_path  +"/"+ file
-		text = open(file_name);
-		for line in text:
-			runtime_file.append(line)
-
-
+	file_name = runtime_path  +"/"+ runtime_file_name
+	text = open(file_name);
+	for line in text:
+		runtime_file.append(line)
+	
 def minus_static_output():
 	for file in os.listdir(static_path):
 		file_name = static_path + "/" + file
 		text = open(file_name)
 		for line in text:
 			if line not in runtime_file:
+				#print line + "not in"
 				static_extra.append(line)
+				#i = i + 1
 
 			if line in runtime_file:
+				#print line + "in"
 				runtime_file.remove(line)
+				#print len(runtime_file)
 
 
 def write_result():
-	static_out = open(output_folder + "/static_REMAINING.txt", "a+")
-	runtime_out = open(output_folder + "/runtime_REMAINING.txt", "a+")
+	runtime_out = open(output_folder + "/runtime_REMAINING.txt", "a")
 
 	for elements in runtime_file:
 		runtime_out.write(elements)
 	runtime_out.close()
+
+	static_out = open(output_folder + "/static_REMAINING.txt", "a")
 
 	for elements in static_extra:
 		static_out.write(elements)
 	static_out.close()
 
 
+
 runtime_path = sys.argv[1]
 static_path = sys.argv[2]
-apk_name = sys.argv[3]
-runtime_file_name = sys.argv[4]
+runtime_file_name = sys.argv[3]
 
 global output_folder
 output_folder = runtime_path
@@ -48,6 +52,7 @@ runtime_file = list();
 static_extra = list();
 
 add_runtime_output()
+
 minus_static_output()
 
 if not runtime_file and not static_extra:
@@ -55,6 +60,7 @@ if not runtime_file and not static_extra:
 else:
 	print "is encrypt"
 	write_result()
+
 
 
 
--- printName.sh
+++ printName.sh
@@ -3,22 +3,29 @@
 apk_dataset=$1
 
 #get target folder
-backsmali_output=$2
+backsmali_output=baksmali
 
 #get runtime output folder
-Folder_runtime_out=$3
+Folder_runtime_out=$2
 
 #location of static anaysis result
-Full_Path_File_Folder=$4
+Full_Path_File_Folder=static
 
 #Folder_runtime_out=$(aapt dump badging "$*" | awk '/package/{gsub("name=|'"'"'","");  print $2}')
 
-cat ./$Folder_runtime_out/*.txt > ./$Folder_runtime_out/catfile.txt
+
 ################################
 #   backsmali for all apk in dataset folder #
 ################################
 for file_a in ${apk_dataset}/*; do
+    mkdir ${backsmali_output}
+    mkdir ${Full_Path_File_Folder}
 	temp_file=`basename $file_a`
+
+	#run_time folder name
+	Folder_runtime_out=$(aapt dump badging "${temp_file}" | awk '/package/{gsub("name=|'"'"'","")}')
+	cat ./$Folder_runtime_out/*.txt > ./$Folder_runtime_out/catfile.txt
+	
 	java -jar baksmali-2.1.0.jar ${apk_dataset}/${temp_file} -o ${backsmali_output}/${temp_file}
 	for sub_folder in ${backsmali_output}/*; do
 		##################################
@@ -31,11 +38,11 @@ for file_a in ${apk_dataset}/*; do
 	####################################
 	#argv[1] directory of runtime analysis output    #
 	#argv[2] directory of static analysis output        #
-	#argv[3] name of the apk                                     #
+	#argv[3] cat all the runtime output                                    #
 	####################################
-	python compare.py ${Folder_runtime_out} ${Full_Path_File_Folder} $temp_file catfile.txt
+	python compare.py ${Folder_runtime_out} ${Full_Path_File_Folder} catfile.txt
 	#reset all the folder, ready to analysis next app
-	#rm -rf ${backsmali_output}/*
-	#rm -rf ${Folder_runtime_out}/*
-	#rm -rf ${Full_Path_File_Folder}/*
+	rm -rf ${backsmali_output}
+	#rm -rf ${Folder_runtime_out}
+	rm -rf ${Full_Path_File_Folder}
 done
